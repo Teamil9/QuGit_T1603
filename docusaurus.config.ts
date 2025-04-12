4f328ef30037e29adf17281e068d9b271300c194
Teamil9 <kisame.opposite@gmail.com>
2025-04-12 09:27:45 +0900

    Commit2504120927

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: 'https://Teamil9.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/QuGit_T1603/docs',
+  baseUrl: '/QuGit_T1603/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
